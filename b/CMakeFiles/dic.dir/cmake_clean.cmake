75d7d65dce6037d4883c7cbca4d590b83fe9b849
minh3087 <nguyengiaminh2k@gmail.com>
2020-06-10 21:30:07 +0700

    update

---
+++ b/CMakeFiles/dic.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dic.dir/dic.c.o"
+  "dic.pdb"
+  "dic"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/dic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
